b597adb476c116dcad000256b2b82e53771f0042
ketewang <ketewang@youyuantech.com>
2018-01-26 14:07:55 +0800

    增加了注释

--- YYKeyenceReaderConsole/ClientSocket.cs
+++ YYKeyenceReaderConsole/ClientSocket.cs
@@ -5,7 +5,9 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Net;
 using System.Net.Sockets;
-
+/// <summary>
+/// Kurt Youyuan
+/// </summary>
 
 namespace YYKeyenceReaderConsole
 {
